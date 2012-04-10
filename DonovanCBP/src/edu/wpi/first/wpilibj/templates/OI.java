e67ae402a0b40e3e4fd88c42e720112969d2b717
Prog694 <prog694@gmail.com>
2012-04-10 11:29:58 -0400

    Makes the OI

--- DonovanCBP/src/edu/wpi/first/wpilibj/templates/OI.java
+++ DonovanCBP/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -1,8 +1,6 @@
-
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -15,30 +13,39 @@ public class OI {
     // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
+
     // Another type of button you can create is a DigitalIOButton, which is
     // a button or switch hooked up to the cypress module. These are useful if
     // you want to build a customized operator interface.
     // Button button = new DigitalIOButton(1);
-    
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
-    
     //// TRIGGERING COMMANDS WITH BUTTONS
     // Once you have a button, it's trivial to bind it to a button in one of
     // three ways:
-    
     // Start the command when the button is pressed and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
-    
     // Run the command while the button is being held down and interrupt it once
     // the button is released.
     // button.whileHeld(new ExampleCommand());
-    
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
-}
+    private Joystick leftstick;
+    private Joystick rightstick;
 
+    public OI() {
+        leftstick = new Joystick(RobotMap.LEFT_JOYSTICK_PORT);
+        rightstick = new Joystick(RobotMap.RIGHT_JOYSTICK_PORT);
+    }
+
+    public Joystick getRightStick() {
+        return rightstick;
+    }
+
+    public Joystick getLeftStick() {
+        return leftstick;
+    }
+}
