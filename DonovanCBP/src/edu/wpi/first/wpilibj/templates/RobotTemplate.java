f2a4a2728cc9a2139f42a856cdc1b181fcb46cdb
Prog 694 <prog694@gmail.com>
2012-06-15 12:47:51 -0400

    Remove ExampleCommand.

--- DonovanCBP/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ DonovanCBP/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.templates.commands.CommandBase;
-import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -31,7 +30,7 @@ public class RobotTemplate extends IterativeRobot {
      */
     public void robotInit() {
         // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
+        // autonomousCommand = new ExampleCommand();
 
         // Initialize all subsystems
         CommandBase.init();
